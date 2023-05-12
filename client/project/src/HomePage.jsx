e2bfcff03954a4ac040c440cf856d359db247c2d
islemsassi19 <salmasassi170@gmail.com>
2023-05-12 17:11:01 +0200

    home

--- client/project/src/HomePage.jsx
+++ client/project/src/HomePage.jsx
@@ -15,7 +15,39 @@ function HomePage() {
       <div className="container">
         <button id="button">Teacher</button>
       </div>
-      <img src="https://media.istockphoto.com/vectors/school-female-teacher-vector-id690861988?k=6&m=690861988&s=170667a&w=0&h=cylgUeOBP8jI66z1qz_Jc-1ehfidjeIepIuBKqxoaZ8=" alt="Logo" />
+      <img src="https://cdn.discordapp.com/attachments/1071021687845421096/1106584507743752252/Untitled_design-removebg-preview.png" alt="Logo" /> <br />
+
+      <video width="640" height="360" autoplay  controls>
+        <source
+          src="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683902406/Untitled_design_1_1_a53s3g.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683902406/Untitled_design_1_1_a53s3g.mp4"
+          type="video/webm"
+        />
+        <p>
+          
+          <a href="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683902406/Untitled_design_1_1_a53s3g.mp4" ></a>
+          instead.
+        </p>
+      </video>
+      <video width="640" height="350" autoplay  controls>
+        <source
+          src="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683904006/Untitled_design_1_sblr8r.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683904006/Untitled_design_1_sblr8r.mp4"
+          type="video/webm"
+        />
+        <p>
+          
+          <a href="https://res.cloudinary.com/dk9hepbrk/video/upload/v1683904006/Untitled_design_1_sblr8r.mp4" ></a>
+          instead.
+        </p>
+      </video>
+
       <footer>
         <p>&copy; 2023 Your Company</p>
       </footer>
